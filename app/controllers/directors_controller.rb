a6a815cae2828837a4a227960687abb8f30374fd
ChandrPr <praphulla91@gmail.com>
2024-11-07 07:49:46 +0000

    completed youngest and eldest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,6 +3,16 @@ class DirectorsController < ApplicationController
     @directors = Director.all
     render({ :template => "director_templates/index" })
   end
+  def youngest
+    @director = Director.where.not({ :dob => nil}).order({:dob => :desc})[0]
+    @case = "youngest"
+    render({ :template => "director_templates/ageist" })
+  end
+  def eldest
+    @director = Director.where.not({ :dob => nil}).order({:dob => :asc})[0]
+    @case = "eldest"
+    render({ :template => "director_templates/ageist" })
+  end
   def show
     @director = Director.where( :id => params[:id])[0]
     @movies = Movie.where(:director_id => params[:id])
