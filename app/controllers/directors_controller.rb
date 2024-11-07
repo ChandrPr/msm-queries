c415b6a817f1abb2c52dcc124ec8e0ad40ef1ebf
ChandrPr <praphulla91@gmail.com>
2024-11-07 06:08:46 +0000

    director details done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,10 @@ class DirectorsController < ApplicationController
     @directors = Director.all
     render({ :template => "director_templates/index" })
   end
+  def info
+    @director = Director.where( :id => params[:id])[0]
+    @movies = Movie.where(:director_id => params[:id])
+    render({ :template => "director_templates/info" })
+  end
+
 end
